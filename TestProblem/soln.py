0e464b7456cc694dee973595a05b4e2aa91c48df
James <jcockburn@Jamess-MBP.home>
2018-03-01 12:47:39 +0000

    Crazy random stuff

--- TestProblem/soln.py
+++ TestProblem/soln.py
@@ -1,19 +1,66 @@
 import sys
+from copy import deepcopy
+from random import randint
 
 
 class Pizza():
+
+    ITERATION_LIMIT = 100
+
     def __init__(self, grid, maxSliceSize, minIngredients):
         self.grid = grid
+        self.initialGrid = deepcopy(grid)
         self.maxSliceSize = maxSliceSize
         self.minIngredients = minIngredients
         self.slices = []
+        self.available_shapes = self.generate_shapes()
+        self.possibleLocations = []
+
+    def generate_shapes(self):
+        shapes = []
+        for i in range(1, self.maxSliceSize + 1):
+            for j in range(1, self.maxSliceSize + 1):
+                if i*j <= self.maxSliceSize:
+                    shapes.append((i, j))
+                    #shapes.append((-i, -j))
+        return shapes
 
     def findBest(self):
         possibleSlicesSolution = []
+        return
+
+    def get_random_shape(self):
+        return self.available_shapes[randint(0, len(self.available_shapes) - 1)]
+
+    def get_random_available_location(self):
+        possible = []
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
+                if self.grid[i][j] != 'X':
+                    possible.append((i, j))
+        self.possibleLocations = possible
+        return possible[randint(0, len(possible) - 1)]
+
+    def possible_to_slice():
+        return bool(self.possibleLocations)
+
+    def greedyApproach(self):
+        for i in range(self.ITERATION_LIMIT):
+            location = self.get_random_available_location()
+            shape = self.get_random_shape()
+            slice = Slice(location[0], location[1], location[0] + shape[0], location[1] + shape[1])
+            if slice.isValidSlice(self.grid, self.minIngredients):
+                self.slices.append(slice)
+                self.cutSlice(slice)           
+
+            if not self.possible_to_slice:
+                break
 
 
-    def cutSlice(self, rowStart, colStart, rowFinish, colFinish):
-        return Slice(rowStart, colStart, rowFinish, colFinish)
+    def cutSlice(self, slice):
+        for row in range(slice.coords[0], slice.coords[2] + 1):
+            for col in range(slice.coords[1], slice.coords[3] + 1):
+                self.grid[row][col] = 'X'
 
     def checkValidity(self, proposedSlices):
         areValidSlices = all(slice.isValidSlice(self.grid, self.minIngredients) for slice in proposedSlices)
@@ -23,6 +70,12 @@ class Pizza():
                 if slice != otherSlice:
                     noOverlaps = noOverlaps and not slice.overlaps(otherSlice)
         return areValidSlices and noOverlaps
+
+    def calculateScore(self):
+        score = 0
+        for slice in self.slices:
+            score += slice.size()
+        return score
 
 class Slice():
     def __init__(self, rowStart, colStart, rowFinish, colFinish):
@@ -37,14 +90,19 @@ class Slice():
         )
 
     def isValidSlice(self, pizzaGrid, minIngredients):
+        outOfBounds = self.coords[0] < 0 or self.coords[1] < 0 or self.coords[2] >= len(pizzaGrid) or self.coords[3] >= len(pizzaGrid[0])
+        if outOfBounds:
+            return False
         countT = 0
         countM = 0
-        for row in range(self.coords[0], self.coords[2] + 1):
-            for col in range(self.coords[1], self.coords[3] + 1):
+        for row in range(self.coords[0], self.coords[2]):
+            for col in range(self.coords[1], self.coords[3]):
                 if pizzaGrid[row][col] == 'T':
                     countT += 1
                 elif pizzaGrid[row][col] == 'M':
                     countM += 1
+                elif pizzaGrid[row][col] == 'X':
+                    return False
         return countT >= minIngredients and countM >= minIngredients
 
     def overlaps(self, anotherSlice):
@@ -85,8 +143,8 @@ def main():
     pizza = readFile(sys.argv[1])
 
     # Some awesome algorithm here
-    pizza.slices = [Slice(0, 0, 2, 1)]
-    print(pizza.slices[0].isValidSlice(pizza.grid, pizza.minIngredients)) 
+    pizza.greedyApproach()
+    print(pizza.calculateScore())
 
     writeFile(sys.argv[2], pizza.slices)
 
